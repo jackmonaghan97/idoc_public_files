245de8a49c7702da16295b5b7ab797e0d95f2be3
Jack Monaghan <jack.monaghan.9710@gmail.com>
2026-03-31 11:27:06 -0500

    Remove exits and admissions

--- idoc_dashboard/data/prison_data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-prison_data = pd.read_csv('data\prison_data.csv')
-prison_data['quarter'] = pd.to_datetime(prison_data['quarter'], format='%Y-%m-%d')
-prison_data = prison_data.sort_values('gen_population', ascending=False)
